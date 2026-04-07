402793a09c856d76d3cba43906d906aaa65d5148
Austin Gatlin <10734306+aegatlin@users.noreply.github.com>
2026-04-07 20:55:03 +1000

    update aliases

--- aliases.sh
+++ aliases.sh
@@ -13,7 +13,9 @@ alias imps='iex -S mix phx.server'
 alias m='mise'
 
 alias c='claude'
+alias cad='claude --allow-dangerously-skip-permissions'
 # this unsets the env var if it exists, meaning it will use subscription by
 # force, anthropics/claude-code#8327
 alias cp='env -u ANTHROPIC_API_KEY claude -p'
+alias cadp='env -u ANTHROPIC_API_KEY cad -p'
 alias cw='claude -w'
