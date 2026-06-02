d51a13770504e44720320cf288242b04d669e58d
Austin Gatlin <10734306+aegatlin@users.noreply.github.com>
2026-06-02 14:43:59 +1000

    remove the cp alias since it conflicts with the cp shell command

--- aliases.sh
+++ aliases.sh
@@ -22,8 +22,7 @@ alias imps='iex -S mix phx.server'
 # ais
 alias c='claude'
 alias cad='claude --allow-dangerously-skip-permissions'
-# this unsets the env var if it exists, meaning it will use subscription by
+# `-u` unsets the env var if it exists, meaning it will use subscription by
 # force, anthropics/claude-code#8327
-alias cp='env -u ANTHROPIC_API_KEY claude -p'
 alias cadp='env -u ANTHROPIC_API_KEY cad -p'
 alias cw='claude -w'
